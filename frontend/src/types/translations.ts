275412f884ce0cf4d76ae9392071294f933ea845
mindr17 <mindr17@yandex.ru>
2023-12-18 14:15:45 +0300

    merge

--- frontend/src/types/translations.ts
+++ frontend/src/types/translations.ts
@@ -1,11 +1,3 @@
 export type translationsType = {
-  // signIn: string;
-  // signUp: string;
-  // signOut: string;
-  // h1: string;
-  // errorBoundaryTitle: string;
-  // errorBoundaryBtn: string;
-  // notFoundTitile: string;
-  // notFoundBtn: string;
   [key: string]: string;
 };
