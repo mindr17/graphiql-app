80f23e463bba1f7f1553317dfb661024b8157fb4
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-12-24 21:37:30 +0300

    fix: change settings in eslint

--- frontend/src/store/reducers/editor/editor-slice.ts
+++ frontend/src/store/reducers/editor/editor-slice.ts
@@ -1,5 +1,4 @@
-import type { PayloadAction } from '@reduxjs/toolkit';
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface EditorState {
   url: string;
--- frontend/src/store/store-hooks.ts
+++ frontend/src/store/store-hooks.ts
@@ -1,21 +1,11 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  TypedUseSelectorHook,
+  useDispatch,
+  useSelector,
+} from 'react-redux';
 
-import editorReducer from './reducers/editor/editor-slice';
+import { AppDispatch, RootState } from './store';
 
-const rootReducer = combineReducers({
-  editor: editorReducer,
-});
-
-export const setupStore = () => {
-  return configureStore({
-    reducer: rootReducer,
-  });
-};
-
-export const store = setupStore();
-
-export type RootState = ReturnType<typeof rootReducer>;
-export type AppStore = ReturnType<typeof setupStore>;
-export type AppDispatch = AppStore['dispatch'];
-
-export default store;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> =
+  useSelector;
