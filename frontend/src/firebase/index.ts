edec0920e6f51e6878b8a3fd3a1ecc74f5a20fd9
mindr17 <mindr17@yandex.ru>
2024-01-03 22:34:47 +0300

    fix: remove firebase

--- frontend/src/firebase/index.ts
+++
@@ -1,9 +0,0 @@
-import { getApp, getApps, initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-
-import { firebaseConfig } from '../configs/firebase';
-
-const app = getApps() ? initializeApp(firebaseConfig) : getApp();
-const auth = getAuth(app);
-
-export { app, auth };
