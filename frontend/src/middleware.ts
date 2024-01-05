90c36efa0025e0b9faa2f5ffbcca645267634b58
mindr17 <mindr17@yandex.ru>
2024-01-05 19:58:55 +0300

    fix: middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/backendProfile', '/protected/:path*', 'profile'],
+  matcher: ['/backendProfile', '/protected/:path*', '/profile'],
 };
